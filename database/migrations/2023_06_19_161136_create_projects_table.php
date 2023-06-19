e01d09167948800b762357d88a11893bac73e009
Pietro Maione <pietromaione9@gmail.com>
2023-06-19 22:25:51 +0200

    first commit

---
+++ database/migrations/2023_06_19_161136_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 30)->unique();
+            $table->string('customer',30)->nullable();
+            $table->text('description');
+            $table->string('url')->nullable();
+            $table->string('img_project');
+            $table->string('img_original');
+            $table->string('slug');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
